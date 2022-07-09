57299b3da2d7894a5fac579c8770292cad9931da
CaaioSB <contato@caiosb.com>
2022-07-08 23:27:45 -0300

    docs: fix js-d1001 documentation comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Button from 'components/Button'
 import { ThemeProvider } from '@mui/material/styles'
 
+<<<<<<< HEAD
 function App() {
   return (
     <ThemeProvider>
       <Button variant='contained'>Hello World</Button>
     </ThemeProvider>
   )
+=======
+/**
+ * Default component that renders the first page.
+ * @return The button with Hello World text.
+ */
+const App = () => {
+  return <Button variant='contained'>Hello World</Button>
+>>>>>>> 0873956 (docs: fix js-d1001 documentation comments)
 }
 
 export default App
