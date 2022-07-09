24576d08dad5f3dca8ceddfc9e65516d75e2cbd7
CaaioSB <contato@caiosb.com>
2022-07-09 02:55:29 -0300

    style: start develop styled button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Button from 'components/Button'
+import { ThemeProvider } from '@mui/material/styles'
 
 function App() {
-  return <Button variant='contained'>Hello World</Button>
+  return (
+    <ThemeProvider>
+      <Button variant='contained'>Hello World</Button>
+    </ThemeProvider>
+  )
 }
 
 export default App
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,16 @@
 import Button from '@mui/material/Button'
 import { styled } from '@mui/material/styles'
+import PropTypes from 'prop-types'
 
 const StyledMUIButton = styled(Button)(() => ({}))
 
 const StyledButton = ({ children, ...props }) => {
   return <StyledMUIButton {...props}>{children}</StyledMUIButton>
+}
+
+StyledButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  props: PropTypes.object
 }
 
 export default StyledButton
