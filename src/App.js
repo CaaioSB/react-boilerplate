08739565e15f0166c52e3dc0e83a5cd588c723d0
CaaioSB <contato@caiosb.com>
2022-07-08 23:27:45 -0300

    docs: fix js-d1001 documentation comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Button from '@mui/material/Button'
 
-function App() {
+/**
+ * Default component that renders the first page.
+ * @return The button with Hello World text.
+ */
+const App = () => {
   return <Button variant='contained'>Hello World</Button>
 }
 
