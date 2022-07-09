aeecc69b7efe2aaf3d08dae9d019724aa0b13415
CaaioSB <contato@caiosb.com>
2022-07-09 03:20:31 -0300

    feat: add themeprovider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import Button from 'components/Button'
+import Button from '@mui/material/Button'
 import { ThemeProvider } from '@mui/material/styles'
 
-<<<<<<< HEAD
-function App() {
-  return (
-    <ThemeProvider>
-      <Button variant='contained'>Hello World</Button>
-    </ThemeProvider>
-  )
-=======
+import theme from 'themes'
+
 /**
  * Default component that renders the first page.
  * @return The button with Hello World text.
  */
 function App() {
-  return <Button variant='contained'>Hello World</Button>
->>>>>>> 0873956 (docs: fix js-d1001 documentation comments)
+  return (
+    <ThemeProvider theme={theme}>
+      <Button variant='contained' color='primary'>
+        Hello World
+      </Button>
+    </ThemeProvider>
+  )
 }
 
 export default App
