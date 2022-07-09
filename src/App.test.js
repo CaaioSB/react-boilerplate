65e9b52f5937298ca4522a9c4c9a666541d4a001
CaaioSB <contato@caiosb.com>
2022-07-09 12:51:29 -0300

    test: test button color

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,9 @@ import { render } from '@testing-library/react'
 it('should render a button with Hello World text', () => {
   const { getByRole } = render(<App />)
   const button = getByRole('button')
+  const buttonStyle = window.getComputedStyle(button)
+  const backgroundColor = buttonStyle['background-color']
 
+  expect(backgroundColor).toEqual('rgb(25, 118, 210)')
   expect(button).toHaveTextContent('Hello World')
 })
