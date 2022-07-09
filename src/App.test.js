149d2ee3d52b7fa2a69a524520c0ae3429225d9c
CaaioSB <contato@caiosb.com>
2022-07-09 00:51:19 -0300

    refactor: fix import sort

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
-import { render } from '@testing-library/react'
-import App from './App'
 import '@testing-library/jest-dom/extend-expect'
+
+import App from './App'
+
+import { render } from '@testing-library/react'
 
 it('should render a button with Hello World text', () => {
   const { getByRole } = render(<App />)
